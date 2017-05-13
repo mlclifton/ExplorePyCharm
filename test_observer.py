d037ab57ee912cd6d587a065916d0d286169ca07
mlc <mike.l.clifton@gmail.com>
2017-05-13 17:53:34 +0100

    Corrections to broadcast() to ensure all subs get the message being broadcast only once.

--- test_observer.py
+++ test_observer.py
@@ -8,46 +8,89 @@ class EventSubscriber:
 	def update(self, event, message):
 		print('sub=[{}] event=[{}] message=[{}]'.format(self.name, event, message))
 		
-		
 class EventPublisher:
 	def __init__(self, events):
 		# maps event names to subscribers
 		# str -> dict
 		self.events = {event: dict()
-		for event in events}
+		               for event in events}
 		
 	def get_subscribers(self, event):
 		return self.events[event]
 		
-	def register(self, event, who, callback=None):
+	def register(self, event: str, who: object, callback=None) -> None:
+		""" subscribe to events here
+		
+		:param event: the name of the even caller is interested in
+		:param who: instance of a class that will be called when the event fires
+		:param callback: method called defaults to update()
+		:return: None
+		"""
 		if callback is None:
 			callback = getattr(who, 'update')
 		self.get_subscribers(event)[who] = callback
 		
-	def unregister(self, event, who):
+	def unregister(self, event: str, who: object) -> None:
+		""" removes a subscriber
+		
+		:param event: the event subscribed to
+		:param who: the subscriber
+		:return: None
+		"""
 		del self.get_subscribers(event)[who]
 		
-	def dispatch(self, event, message):
+	def dispatch(self, event: str, message: str) -> None:
+		""" send message to all subscribers of event
+		
+		:param event: the event
+		:param message: the message to send
+		:return: None
+		"""
 		for subscriber, callback in self.get_subscribers(event).items():
 			callback(event, message)
 			
-	def broadcast(self, message):
+	def broadcast(self, message: str) -> int:
+		""" send specified message to all subscribers
+		
+		:param message: the message to send
+		:return: the number of distinct subscribers that got a messsage
+		"""
+		subs_notified_so_far = []
+		receiver_count = 0
 		for event in self.events:
-			self.dispatch(event, message)
-			
-			
+			for subscriber, callback in self.get_subscribers(event).items():
+				if subscriber not in subs_notified_so_far:
+					callback("broadcast", message)
+					subs_notified_so_far.append(subscriber)
+					receiver_count += 1
+		return receiver_count
+
+
+class TestObserver():
+	def __init__(self, name):
+		self.name = name
+		self.message_history = []
+
+	def update(self, event: str, message: str) -> None:
+		self.message_history.append(message)
+
+
 def test_broadcast(capsys):
 	ep = EventPublisher(["One", "Two", "Three"])
-	s1 = EventSubscriber("s1")
+	s1 = TestObserver("s1")
+	s2 = TestObserver("s2")
 	ep.register("One", s1)
 	ep.register("Two", s1)
 	ep.register("Three", s1)
-	ep.broadcast("Hello")
-	out, err = capsys.readouterr()
-	assert out == "sub=[s1] event=[One] message=[Hello]\n"\
-	"sub=[s1] event=[Two] message=[Hello]\n"\
-	"sub=[s1] event=[Three] message=[Hello]\n"
-	
+	ep.register("Three", s2)
+	receiver_count = ep.broadcast("Hello")
+	assert receiver_count == 2
+	assert len(s1.message_history) == 1
+	assert len(s2.message_history) == 1
+	assert "Hello" in s1.message_history
+	assert len(s2.message_history) == 1
+	assert "Hello" in s2.message_history
+
 	
 def test_single_pub_single_sub_subscriber(capsys):
 	ep = EventPublisher(["EventOne"])
